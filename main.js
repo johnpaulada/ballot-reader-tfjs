36a0492436b8935558b362e617a18c5f4684796d
John Paul Ada <johnpaulada@Johns-Mac-mini.local>
2018-06-05 12:28:06 +0800

    Added Parcel

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ const directories = [
     'quezon',
     'tacloban'
 ]
+
+const filesInDirectories = directories.map(d => {
+
+})
+
+/**
+ * Pad string with zeros on the left.
+ * 
+ * @param {String} str String to pad
+ * @param {Number} x Number of characters expected 
+ */
+function zeroPad(str, x) {
+  
+}
