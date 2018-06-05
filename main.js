e7d7ca5c5c9d3e7d4ea63f532214acdf26a07ce3
John Paul Ada <johnpaulada@Johns-Mac-mini.local>
2018-06-05 14:25:37 +0800

    Get images

--- main.js
+++ main.js
@@ -6,24 +6,53 @@ const directories = [
     'tacloban'
 ]
 
-const filesInDirectories = directories.map(d => {
-  let i = 1
-  return Array(30).fill(1).map(() => `${d}-${zeroPad(`${i++}`, 2)}.jpg`)
-})
-
-const imageFiles = filesInDirectories.reduce((files, lst) => {
-  return [...lst, ...files]
-}, [])
-
-console.log(imageFiles)
-
 /**
- * Pad string with zeros on the left.
+ * Pads a string with zeros on the left.
  * 
  * @param {String} str String to pad
  * @param {Number} x Number of characters expected 
  */
-function zeroPad(str, x) {
+const zeroPad = (str, x) => {
   const remaining = x - str.length
   return `${"0".repeat(remaining)}${str}`
 }
+
+const filesInDirectories = directories.map(d => {
+  let i = 1
+  return Array(30).fill(1).map(() => `/data/${d}-${zeroPad(`${i++}`, 2)}.jpg`)
+})
+
+const imageFilenames = filesInDirectories.reduce((files, lst) => {
+  return [...lst, ...files]
+}, [])
+
+/**
+ * Gets the image from the URL and constructs an Image object.
+ * 
+ * @param {String} src URL of image 
+ */
+const getImageFromUrl = async (src) => {
+  const image = new Image(1700, 2800)
+  image.src = src
+  image.crossOrigin = "Anonymous"
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext("2d")
+
+  return await new Promise((resolve, reject) => {
+    image.onload = () => {
+      canvas.width = image.width
+      canvas.height = image.height
+      ctx.drawImage(image, 0, 0)
+      image.src = canvas.toDataURL("image/jpeg")
+      resolve(image)
+    }
+  })
+}
+
+const getData = async () => {
+  console.log("LOL")
+  const imageObjects = await Promise.all(imageFilenames.map(filename => getImageFromUrl(filename)))
+  console.log(imageObjects)
+}
+
+getData()
