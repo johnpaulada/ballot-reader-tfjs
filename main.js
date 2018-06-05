39c87926d4d9f08d55a743b0bd054901b98cfaa5
John Paul Ada <johnpaulada@Johns-Mac-mini.local>
2018-06-05 13:48:39 +0800

    Created image list

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ const directories = [
 ]
 
 const filesInDirectories = directories.map(d => {
-
+  let i = 1
+  return Array(30).fill(1).map(() => `${d}-${zeroPad(`${i++}`, 2)}.jpg`)
 })
+
+const imageFiles = filesInDirectories.reduce((files, lst) => {
+  return [...lst, ...files]
+}, [])
+
+console.log(imageFiles)
 
 /**
  * Pad string with zeros on the left.
@@ -17,5 +24,6 @@ const filesInDirectories = directories.map(d => {
  * @param {Number} x Number of characters expected 
  */
 function zeroPad(str, x) {
-  
+  const remaining = x - str.length
+  return `${"0".repeat(remaining)}${str}`
 }
